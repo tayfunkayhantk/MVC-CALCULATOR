6b512408361bc84ad46b734694b30007da7816bb
tayfunkayhantk <tayfunkayhantk@gmail.com>
2022-06-24 04:28:54 +0300

    MVC CALCULATOR
    MVC ile yaptığım hesap makinesi projesi

---
+++ MVC hesap makinesi/MVC hesap makinesi/Controllers/HesapMakinesiController.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MVC_hesap_makinesi.Controllers
+{
+    public class HesapMakinesiController : Controller
+    {
+        // GET: HesapMakinesi
+        
+           public ActionResult HesapMakinesi(int sayi1 = 0, int sayi2 = 0, string islem123 = "+")
+        {
+            int sonuc = 0;
+            switch (islem123)
+            {
+                case "+":
+                    sonuc = sayi1 + sayi2;
+                    break;
+                case "-":
+                    sonuc = sayi1 - sayi2;
+                    break;
+                case "*":
+                    sonuc = sayi1 * sayi2;
+                    break;
+                case "/":
+                    sonuc = sayi1 / sayi2;
+                    break;
+            }
+
+            ViewBag.snc = sonuc;
+            return View();
+        }
+    }
+}
---
+++ MVC hesap makinesi/MVC hesap makinesi/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="MVC_hesap_makinesi.MvcApplication" Language="C#" %>
